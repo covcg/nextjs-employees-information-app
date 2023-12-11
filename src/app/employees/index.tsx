926ca6bfb0d5a93284bfd7b93280ce2b70ab7b76
covcg <coello_cg95@hotmail.com>
2023-12-10 22:34:04 -0600

    additional config

--- src/app/employees/index.tsx
+++
@@ -1,6 +0,0 @@
-import EmployeesPage from "@/pages/employees";
-import React from "react";
-
-const EmployeesRoute = () => <EmployeesPage />;
-
-export default EmployeesRoute;
---
+++ src/app/employees/page.tsx
@@ -0,0 +1,6 @@
+import EmployeesPage from "@/pages/employees";
+import React from "react";
+
+const Page = () => <EmployeesPage />;
+
+export default Page;
